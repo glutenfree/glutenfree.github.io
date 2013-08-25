a3ef5bdcae93e4021a56b9d2456bbc296795b570
lena <savelyeva.elen@gmail.com>
2013-08-25 19:31:58 +0400

    Sitemap

---
+++ _plugins/_cat_gen.rb
@@ -0,0 +1,38 @@
+module Jekyll
+
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['category'] = category
+
+      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      self.data['title'] = "#{category_title_prefix}#{category}"
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category_index'
+        dir = site.config['category_dir'] || 'categories'
+        site.categories.keys.each do |category|
+         write_category_index(site, File.join(dir, category),category)
+        end
+      end
+    end
+   def write_category_index(site, dir, tag)
+      index = CategoryPage.new(site, site.source, dir, tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+  end
+
+end
